a23170b45a9f078d957923609e2233ca6679173e
Choiyoungwon0825 <fnul1005@naver.com>
2024-07-05 09:42:06 +0900

    first commit

---
+++ src/main/java/com/parkjava/ParkjavaApplication.java
@@ -0,0 +1,13 @@
+package com.parkjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkjavaApplication.class, args);
+	}
+
+}
