f5a555390e8d710aa27cfc270ec61b048132555d
Choiyoungwon0825 <fnul1005@naver.com>
2024-07-23 17:28:02 +0900

    user부분에서 db조회 가능한 항목 되도록 추가

---
+++ parkjava/src/main/java/com/parkjava/controller/userInquiryController.java
@@ -0,0 +1,50 @@
+package com.parkjava.controller;
+
+import com.parkjava.model.inquiryModel;
+import com.parkjava.repository.inquiryRepository;
+import com.parkjava.service.inquiryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/user/api/inquiry")
+public class userInquiryController {
+
+    @Autowired
+    private inquiryService inquiryService;
+    @Autowired
+    private inquiryRepository inquiryRepository;
+
+    @GetMapping
+    public List<inquiryModel> inquiryDESCAll() {
+        return inquiryRepository.inquiryDESCAll();
+    }
+
+    @GetMapping("/{inquiryIndex}")
+    public ResponseEntity<inquiryModel> getUserById(@PathVariable Long inquiryIndex) {
+        inquiryModel inquiry = inquiryService.getUserById(inquiryIndex);
+        if (inquiry == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(inquiry);
+    }
+
+    @PostMapping("/create")
+    public inquiryModel createUser(@RequestBody inquiryModel inquiry) {
+        return inquiryService.createUser(inquiry);
+    }
+
+    @PutMapping("/update/{inquiryIndex}")
+    public ResponseEntity<inquiryModel> updateUser(@PathVariable Long inquiryIndex, @RequestBody inquiryModel inquiryDetails) {
+        inquiryModel updatedTest = inquiryService.updateUser(inquiryIndex, inquiryDetails);
+        if (updatedTest == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedTest);
+    }
+
+}
